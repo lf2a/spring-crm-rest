8f855a3a5374413cd5702f55ec2cab28af18b395
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-09-05 13:12:48 -0300

    login system

---
+++ src/main/java/com/lf2a/springdemo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.lf2a.springdemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
