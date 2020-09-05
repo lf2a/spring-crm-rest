9fc21976579f9d5c71f03a6666d465787252a432
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-09-05 11:30:56 -0300

    First commit - configuration

---
+++ src/main/java/com/lf2a/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.lf2a.springdemo.service;
+
+import java.util.List;
+
+import com.lf2a.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
