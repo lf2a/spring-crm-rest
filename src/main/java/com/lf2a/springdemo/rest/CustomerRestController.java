a56d6b3b00d898efe36d4072af73623b4b2d169c
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-09-05 12:41:19 -0300

    add customer

--- src/main/java/com/lf2a/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/lf2a/springdemo/rest/CustomerRestController.java
@@ -3,10 +3,7 @@ package com.lf2a.springdemo.rest;
 import com.lf2a.springdemo.entity.Customer;
 import com.lf2a.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +26,15 @@ public class CustomerRestController {
         if (customer == null) {
             throw new CustomerNotFoundException("Customer id not found - " + customerId);
         }
+
+        return customer;
+    }
+
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer customer) {
+        customer.setId(0);
+        // se o id for 0 ou null, ele irá adicionar no banco de dados, caso contrário ele ira atualizar
+        customerService.saveCustomer(customer);
 
         return customer;
     }
